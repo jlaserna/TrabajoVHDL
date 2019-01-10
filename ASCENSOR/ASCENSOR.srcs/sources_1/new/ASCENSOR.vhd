1dd728b94daec257a8637e7fefcf31f9b1f7c026
lpedrero <lpedrerom03.iesvj@gmail.com>
2019-01-10 10:21:37 +0100

    Ascensor 0.2v

--- ASCENSOR/ASCENSOR.srcs/sources_1/new/ASCENSOR.vhd
+++ ASCENSOR/ASCENSOR.srcs/sources_1/new/ASCENSOR.vhd
@@ -1,43 +1,57 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09.01.2019 10:15:37
--- Design Name: 
--- Module Name: ASCENSOR - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ASCENSOR is
---  Port ( );
+     Port ( clk : in std_logic;
+            reset : in std_logic;
+            piso_deseado : in std_logic_vector (1 downto 0);
+            puerta_abierta : out std_logic;
+            display_number : out std_logic_vector (6 downto 0);
+            display_selection : out std_logic_vector (3 downto 0)
+     );
 end ASCENSOR;
 
-architecture Behavioral of ASCENSOR is
-
+architecture structural of ASCENSOR is
+signal clk_sinc : std_logic;
+signal piso_actual : std_logic_vector(1 downto 0);
+     component Maquina_Estado is
+     port ( clk : in std_logic;
+            reset : in std_logic;
+            piso_deseado : in std_logic_vector (1 downto 0);
+            piso_nuevo : out std_logic_vector (1 downto 0);
+            puerta: out std_logic;
+            motor: out std_logic_vector(1 downto 0);
+            tmp: out std_logic_vector(1 downto 0)
+     );
+     end component;
+     
+     component Clk_divider is
+     port ( clk,reset: in std_logic;
+            clk_out: out std_logic
+     );
+     end component;
+     
+     component BCD is
+     port ( code : in std_logic_vector (3 downto 0);
+            led : out std_logic_vector (6 downto 0)
+     );
+     end component;
+      
 begin
-
-
-end Behavioral;
+     inst_Maquina_Estado: Maquina_Estado port map(
+        clk=>clk_sinc,
+        reset=>reset,
+        piso_deseado=>piso_deseado,
+        piso_nuevo=>piso_actual,
+        puerta=>puerta_abierta
+     );
+     inst_Clk_divider: Clk_divider port map(
+        clk=>clk,
+        reset=>reset,
+        clk_out=>clk_sinc
+     );
+     inst_BCD: BCD port map(
+        led=>display_number,
+        code=>piso_actual
+     );
+end;
